70ae7ed772719e96b4ab594fb7905f929f25707d
Evan Ang <alethiologist@gmail.com>
2020-06-29 19:21:06 +0800

    fixed header and too long text

--- .history/db/seeds_20200629191631.rb
+++ .history/db/seeds_20200629191631.rb
@@ -9,7 +9,7 @@
 Item.create(name: 'Olympic size pool', volume: 660_000, unit: 'gallons', symbol: 'swimmer')
 Item.create(name: 'tub of ice cream', volume: 16, unit: 'ounces', symbol: 'ice_cream')
 Item.create(name: 'professional ice rink', volume: 10_600, unit: 'gallons', symbol: 'ice_skate')
-Item.create(name: 'large oil drum', volume: 55, unit: 'gallons', symbol: 'oil_drum')
+Item.create(name: 'full oil drum', volume: 55, unit: 'gallons', symbol: 'oil_drum')
 Item.create(name: 'cask of amontillado', volume: 126, unit: 'gallons', symbol: 'wine_glass')
 Item.create(name: 'tank of gas', volume: 15, unit: 'gallons', symbol: 'fuelpump')
 Item.create(name: 'average burrito', volume: 750, unit: 'millilitres', symbol: 'burrito')
